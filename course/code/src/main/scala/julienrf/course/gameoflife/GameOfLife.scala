306fa78d59a1d0453cb1b2b2c28eae865be25cd1
Vincent Debergue <vincent.debergue@fabernovel.com>
2017-11-23 16:24:34 +0100

    add readme for gol

--- course/code/src/main/scala/julienrf/course/gameoflife/GameOfLife.scala
+++ course/code/src/main/scala/julienrf/course/gameoflife/GameOfLife.scala
@@ -3,7 +3,16 @@ package julienrf.course.gameoflife
 object GameOfLife {
 
   def start(size: Int) {
+    val clock: Events[Unit] = Events.every(200)
+    val worldInit: World = World.init(size)
+    // val worldInit: World = World.test
 
+
+    val worlds: Events[World] = ???
+
+    val images: Events[Image] = worlds.map(???)
+
+    Ui.show(images)
   }
 
 }
